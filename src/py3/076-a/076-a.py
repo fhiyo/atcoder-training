9c9bfdcb9f8aca547fd0d9af22453008cad3ac00
fhiyo <fhiyo1201@gmail.com>
2017-10-29 10:51:41 +0900

    Add 076a-c (Only py3)

---
+++ src/py3/076-a/076-a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""076-a"""
+
+
+import sys
+
+
+def main():
+    """Main function."""
+    R = int(input())
+    G = int(input())
+
+    print(2 * G - R)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
