7fe60a646e5c4811313f971b32d59942755b8e69
fhiyo <f.hyodo@opt.ne.jp>
2020-07-25 21:07:01 +0900

    update

---
+++ util/python/copy_code_and_open_page.py
@@ -0,0 +1,63 @@
+import subprocess
+import sys
+from typing import List
+import webbrowser
+from collections import namedtuple
+
+import requests
+
+from Config import SRC_PATH
+from download_samples import login, problemIdFromUrl
+
+
+Lang = namedtuple('Lang', ('name', 'extension', 'comment_pattern'))
+
+LANGS = {
+    # XXX: 本当はcomment_pattern正規表現にしたいけどサボる
+    'cpp17': Lang('cpp17', 'cpp', '// '),
+}
+
+
+def get_formatted_code(probmem_number: str, lang: Lang) -> List[str]:
+    # 自分のコードを取得し、コメントアウトを除き、EOF前のwhitespaceを捨てる
+    source_file_path = SRC_PATH / lang.name / f'{probmem_number}/{probmem_number}.{lang.extension}'
+    with open(source_file_path) as f:
+        # NOTE: 末尾の改行コードを削るために配列の末尾だけ無視している
+        source = list(map(lambda l: l[:-1], filter(lambda l: not l.startswith(lang.comment_pattern), f.readlines())))
+        line_idx = len(source)  # 1-index
+        for line in reversed(source):
+            if line != '':
+                break
+            line_idx -= 1
+
+    # XXX: macOSXのみ
+    subprocess.run("pbcopy", universal_newlines=True, input='\n'.join(source[:line_idx]))
+
+
+def main(probmem_number: str, lang_name: str, open: bool):
+    if not lang_name in LANGS.keys():
+        raise NotImplementedError(f"Invalid lang.\n Supported language: {list(LANGS.keys())}")
+
+    contest_name = probmem_number.partition('_')[0]
+    url = f'https://atcoder.jp/contests/{contest_name}/tasks/{probmem_number}'
+    get_formatted_code(probmem_number, LANGS[lang_name])
+
+    if open:
+        # loginする
+        problem_id = problemIdFromUrl(url)
+        session = requests.Session()
+        login(session, problem_id)
+        # ページに遷移するだけ
+        webbrowser.open(url + '#select-lang')
+
+
+if __name__ == '__main__':
+    # TODO: 汚いのでもうちょい整理したい
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--problem_number', type=str, help='contest problem number (ex. abc155_a)')
+    parser.add_argument('--lang', '-l', type=str, help='language')
+    parser.add_argument('--open', action='store_true', help='open problem page')
+    args = parser.parse_args()
+
+    sys.exit(main(args.problem_number, args.lang, args.open))
